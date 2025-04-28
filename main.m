6db7921fbeaa3fbb9a0c2255ef3a35bd8aec8cdb
yyyppt <3337402293@qq.com>
2025-04-28 17:25:01 +0800

    first_code

---
+++ main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  new
+//
+//  Created by yyyyy on 2025/4/27.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
